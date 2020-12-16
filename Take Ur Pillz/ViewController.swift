7d06eed13845fe71043f228575b3f08092a3bd49
Max094Reikeb <maxencebekier@gmail.com>
2020-12-17 00:40:27 +0100

    Initial Commit

---
+++ Take Ur Pillz/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Take Ur Pillz
+//
+//  Created by Maxence Bekier on 17/12/2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
