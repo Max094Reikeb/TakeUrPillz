36183ab1f726fc8d4760b4aea97ea203a2261855
Max094Reikeb <maxencebekier@gmail.com>
2020-12-21 18:38:01 +0100

    Modified global architecture, main button, and detail page

--- Take Ur Pillz/Controllers/InfosViewController.swift
+++ Take Ur Pillz/Controllers/InfosViewController.swift
@@ -9,10 +9,14 @@ import UIKit
 
 class InfosViewController: UIViewController {
 
+    @IBOutlet weak var returnButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
         // Do any additional setup after loading the view.
     }
-
+    
+    @IBAction func returnAction(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
 }
